84d433f58b378f066c9dbbe4ba5e60f5f8857ae5
Andrew Cox <drew.r.cox@gmail.com>
2017-02-24 13:43:07 -0500

    Site now functional. Added some styling. Implementing lightbox

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
 var YOUTUBE_BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
+var nextToken;
+var prevToken;
+var query;
 
-function getDataFromApi(searchTerm, callback) {
+function getDataFromApi(searchTerm, pageToken, callback) {
   var query = {
     part: 'snippet',
     key: 'AIzaSyDVTdsV_xm2uLcpouPg-tV259WE274ketQ',
-    q: searchTerm
+    q: searchTerm,
+    pageToken: pageToken
   }
   $.getJSON(YOUTUBE_BASE_URL, query, callback);
 }
 
 
 function displayTUBESearchData(data) {
+  console.log(data);
+  nextToken = data.nextPageToken;
+  prevToken = data.prevPageToken;
+  var num = 0;
   var resultElement = '';
-  if (data.Search) {
-    data.Search.forEach(function(item) {
-     resultElement += '<p>' + item.Title + '</p>';
+  if (data.items) {
+    data.items.forEach(function(item) {
+     resultElement += '<div class="results">' +
+                        '<h3><a href="https://www.youtube.com/watch?v=' + item.id.videoId + '" target="_blank">' + item.snippet.title + '</a></h3>' +
+                        '<div class="description">' +
+                          '<div class="thumbnail">' +
+                            '<a href="https://www.youtube.com/watch?v=' + item.id.videoId + '"><img src=' + item.snippet.thumbnails.default.url + ' data-lightbox="image' + num + '"></a>' +
+                          '</div>' +
+                          '<div class="info">' +
+                            '<h5>Uploaded by <a href="https://www.youtube.com/channel/' + item.snippet.channelId + '" target="_blank">' + item.snippet.channelTitle + '</a></h5>' +
+                            '<p>' + item.snippet.description + '</p>' +
+                          '</div>' +
+                        '</div>' +
+                      '</div>' +
+                      '<div style="clear:left"></div>';
+      num +=1;
     });
   }
   else {
@@ -27,8 +48,14 @@ function displayTUBESearchData(data) {
 function watchSubmit() {
   $('.js-search-form').submit(function(e) {
     e.preventDefault();
-    var query = $(this).find('.js-query').val();
-    getDataFromApi(query, displayTUBESearchData);
+    query = $(this).find('.js-query').val();
+    getDataFromApi(query, null, displayTUBESearchData);
+  });
+  $('#prev').click(function(event) {
+    getDataFromApi(query, prevToken, displayTUBESearchData);
+  });
+  $('#next').click(function(event) {
+    getDataFromApi(query, nextToken, displayTUBESearchData);
   });
 }
 
